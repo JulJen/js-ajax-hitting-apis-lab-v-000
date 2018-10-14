38b3d2006e751e56d9bcb03ab0e06c7b387a213d
Jul Jen <juliajen.prog@gmail.com>
2018-10-14 01:23:13 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -1,5 +1,27 @@
 // your code here
 
+function showRepositories() {
+
+  // tell JavaScript that it's working with JSON
+  var repos = JSON.parse(this.responseText);
+  console.log(repos);
+  // parsing the text into an array of objects
+
+  const repoList = `<ul>${repos
+    .map(
+      r =>
+        '<li>' +
+        r.name +
+        ' - <a href="#" data-repo="' +
+        r.name +
+        '" onclick="getCommits(this)">Get Commits</a></li>'
+    )
+    .join('')}</ul>`;
+  //data attribute to hold the repo name
+  document.getElementById('repositories').innerHTML = repoList;
+}
+
+
 function displayRepositories() {
   // creating a new instance of an XMLHttpRequest
   // initiate XHR request
