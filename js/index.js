f6eeebc1970fd45f470ddf5561e7236358279eb0
Jul Jen <juliajen.prog@gmail.com>
2018-10-14 01:22:47 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
 // your code here
+
+function displayRepositories() {
+  // creating a new instance of an XMLHttpRequest
+  // initiate XHR request
+  const req = new XMLHttpRequest();
+
+  // add event listener to our req object, this will be our req object inside our callback function.
+  req.addEventListener('load', showRepositories);
+
+  // call open with the HTTP verb GET and the URI for request.
+  req.open('GET', 'https://api.github.com/users/octocat/repos');
+  req.send();
+}
